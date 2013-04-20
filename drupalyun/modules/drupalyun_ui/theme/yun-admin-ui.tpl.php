41ea0c342043a90fc93e84ee12d0505b41a29b96
jackey <jziwenchen@gmail.com>
2013-04-20 13:49:31 +0800

    begin todo drupalyun_ui

---
+++ drupalyun/modules/drupalyun_ui/theme/yun-admin-ui.tpl.php
@@ -0,0 +1,16 @@
+<div id="core-nav" class="yun-admin-ui">
+	<ul id="core-nav-menu">
+		<li class="core-nav-link core-nav-link-1"><?php print t("Core Config")?></li>
+		<li class="core-nav-link core-nav-link-2"><?php print t("SEO Config")?></li>
+	</ul>
+</div>
+<div id="core-sub-nav" class="yun-admin-ui">
+	<ul id="core-sub-nav-menu"></ul>
+	<li class="core-sub-nav-link core-sub-nav-link-1"><?php print t("Main Menu/Page Manage")?></li>
+	<li class="core-sub-nav-link core-sub-nav-link-2"><?php print t("Build Site Into HTML Static")?></li>
+</div>
+<div id="core-nav-content" class="yun-admin-ui">
+	TODO:
+</div>
+
+<div class="clearfix"></div>
